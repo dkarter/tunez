f4b169bd7603a21755040088570989f292161b17
Dorian Karter <dkarter@users.noreply.github.com>
2025-09-14 01:20:10 -0500

    feat: add validations to albums

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -25,6 +25,19 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  validations do
+    validate numericality(:year_released,
+               greater_than_or_equal_to: 1900,
+               less_than_or_equal_to: &__MODULE__.next_year/0
+             ),
+             where: [present(:year_released)],
+             message: "must be between 1900 and next year"
+
+    validate match(:cover_image_url, ~r/^https?:\/\/[^\s]+$/),
+      where: [changing(:cover_image_url)],
+      message: "must be a valid URL starting with http:// or https://"
+  end
+
   attributes do
     uuid_v7_primary_key :id
 
@@ -38,5 +51,13 @@ defmodule Tunez.Music.Album do
 
   relationships do
     belongs_to :artist, Tunez.Music.Artist, allow_nil?: false
+  end
+
+  identities do
+    identity :unique_album_per_artist, [:name, :artist_id]
+  end
+
+  def next_year do
+    Date.utc_today().year + 1
   end
 end
