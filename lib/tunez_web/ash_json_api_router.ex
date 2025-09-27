40086508e5bfdf8c2ca708363a387e0571bc1d79
Dorian Karter <dkarter@users.noreply.github.com>
2025-09-27 00:38:19 -0500

    docs: add api title and version

--- lib/tunez_web/ash_json_api_router.ex
+++ lib/tunez_web/ash_json_api_router.ex
@@ -2,5 +2,7 @@ defmodule TunezWeb.AshJsonApiRouter do
   @moduledoc false
   use AshJsonApi.Router,
     domains: [Tunez.Music],
-    open_api: "/open_api"
+    open_api: "/open_api",
+    open_api_title: "Tunez API",
+    open_api_version: to_string(Application.spec(:tunez, :vsn))
 end
