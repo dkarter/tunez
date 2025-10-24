98146bf83ffa8026d93ec5fa3a010dfa3eb28da3
Dorian Karter <dkarter@users.noreply.github.com>
2025-10-23 23:44:10 -0500

    feat: add ash_authentication + password strategy

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "0nKY1INKMjsxFHdKQVuU64AfHX2tRGzV"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true
 
 # Configure your database
